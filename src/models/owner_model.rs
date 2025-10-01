53a693ac50f81ed5069bf48cbff74e005ea422d5
Yohanes Christian <yohanesdevano90@gmail.com>
2025-10-01 22:02:19 +0700

    update

---
+++ src/models/owner_model.rs
@@ -0,0 +1,37 @@
+use std::time::SystemTime;
+
+use chrono::Utc;
+use mongodb::bson::{oid::ObjectId, DateTime};
+
+#[derive(Debug, serde::Serialize, serde::Deserialize)]
+
+pub struct Owner {
+    pub _id: ObjectId, 
+    pub name: String,
+    pub email: String,
+    pub phone: String,
+    pub address: String,
+}
+
+#[derive(Debug, serde::Serialize, serde::Deserialize)]
+
+pub struct OwnerRequest {
+    pub name: String,
+    pub email: String,
+    pub phone: String,
+    pub address: String,
+}
+
+impl TryFrom<OwnerRequest> for Owner {
+    type Error = Box<dyn std::error::Error>;
+
+    fn try_from(item: OwnerRequest) -> Result<Self, Self::Error> {
+        Ok(Self {
+            _id: ObjectId::new(),
+            name: item.name,
+            email: item.email,
+            phone: item.phone,
+            address: item.address,
+        })
+    }
+}
